64938f37b7dcf6e8ba3a72b940285435bb55f6cf
Matheus Franklin <matheusjfranklin@gmail.com>
2022-11-15 12:18:39 -0300

    ControlThread implemented

--- motor_control.py
+++ motor_control.py
@@ -14,10 +14,11 @@ class Motor (threading.Thread):
         # Simulation parameters
         self.T = 0.01  # sampling period
         # motor parameters
-        self.V = 20  # armature voltage(input)
+        self.Vmax = 20  # maximum armature voltage
+        self.V = 0  # armature voltage(input)
         self.Ra = 0.78  # armature circuit resistance
         self.La = 0.016  # armature circuit inductance
-        self.Ia = 1  # armature current
+        self.Ia = 1  # armature current #I CANT DECLARE THE CURRENT LIKE THIS. NEED TO FIX THIS LATER
         self.Km = 1  # torque constant
         self.Tm = 0  # motor's torque current value
         self.TL = 0  # load torque (disturbance)
@@ -25,14 +26,16 @@ class Motor (threading.Thread):
         self.B = 0.01  # viscous friction
         self.Wm = 0  # output shaft's angular speed (output)
         self.Kb = 1  # electric constant
+        self.Wmax = (self.Vmax-self.Ia*self.Ra)/self.Kb #maximum speed of the motor
+        self.setPoint = []
         self.y = []
         self.x = []
-        self.iterations = 30
+        self.iterations = 3000
 
     def run(self):
         index = count()
         # motor's difference equations
-        # for i in range(self.iterations):
+        #for i in range(self.iterations):
         while(1):
             self.Tm = ((self.Km)*(self.V)-(self.Km*self.Kb*self.Wm) -
                        (self.Ra*self.Tm))*(self.T/self.La)+self.Tm
@@ -41,7 +44,9 @@ class Motor (threading.Thread):
             # adds the speed values to an array that is used to plot the graph
             self.y.append(self.Wm)
             self.x.append(next(index)*(self.T)) #appends the sampling equivalent time to the x axis
+            self.setPoint.append(self.Wmax/2) #the setpoint vector needs to be the same size of the y vector so they can be plotted together
             #print(self.Wm)
+            #print(self.V)
             sleep(self.T)
 
 
@@ -50,6 +55,15 @@ class ControlThread (threading.Thread):
         threading.Thread.__init__(self)
         # simulation parameters
         self.T = 0.2  # sampling time
+        self.setPoint = motor_thread[0].Wmax/2
+        self.controlSignal = 0
+        self.Kp = 1
+        self.error = 0
+    def run(self):
+        self.error = self.setPoint - motor_thread[0].Wm
+        self.controlSignal = self.error*self.Kp
+        motor_thread[0].V = self.controlSignal
+        print(motor_thread[0].V)
     # not finished yet. Still need to implement "run"
     # does the motors' speed control with T=200ms
     # has a simple control law that keeps the motors working with half of the maximum speed for one minute
@@ -65,6 +79,8 @@ for i in range(30):
 motor_thread[0].start()
 #motor_thread[0].join()
 
+softPLC = ControlThread()
+softPLC.start()
 # Starting new threads
 # for i in range(29):
 #   motor_thread[i].start()
@@ -77,13 +93,15 @@ motor_thread[0].start()
 plt.style.use('fivethirtyeight')
 
 def animate(i):
-  x = motor_thread[0].x
-  y = motor_thread[0].y
+   x = motor_thread[0].x
+   y1 = motor_thread[0].y
+   y2 = motor_thread[0].setPoint
 
-  plt.cla()
-  plt.plot(x, y, label='Motor 0')
-  plt.legend(loc='upper left')
-  plt.tight_layout()
+   plt.cla()
+   plt.plot(x, y1, label='Motor 0')
+   plt.plot(x, y2, label='Setpoint')
+   plt.legend(loc='upper left')
+   plt.tight_layout()
 
 ani = FuncAnimation(plt.gcf(), animate, interval = 1)
 
